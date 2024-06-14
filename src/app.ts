c66ca3f86872a4b209916fd423f0c327532dd7e7
Mike Jovanovich <mpjovanovich@gmail.com>
2024-06-14 17:01:52 -0400

    Able to read dynamic macro file and run function from it at this point.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,3 @@
-console.log("test");
+import { loadMacros } from "./macroLoader.js";
+
+loadMacros("/home/mpjovanovich/git/macro-md/sandbox/testMacro.js");
--- src/macroLoader.ts
+++ src/macroLoader.ts
@@ -1,40 +1,28 @@
 import fs from "fs";
-import path from "path";
 
-const _MACRO_IDENTIFIER = "macro_identifier";
+const MACRO_IDENTIFIER = "macro_identifier";
+type Macro = (...args: any[]) => any;
+const functionMap = new Map<string, Macro>();
 
-// // export interface MacroFunction {
-// //   (args: string[], text: string): string;
-// // }
-
-// interface ModuleExports {
-//   [key: string]: {
-//     name: string;
-//     [key: string]: any;
-//   };
-// }
-
-export function loadMacros(macrosPath: string): void {
-  // export function loadMacros(macrosPath: string): Map<string, MacroFunction> {
+export async function loadMacros(macrosPath: string): Promise<void> {
+  /*
+   * LOAD MACRO FILE
+   */
   // Check if the file exists
   if (!fs.existsSync(macrosPath)) {
     throw new Error(`File does not exist: ${macrosPath}`);
   }
+  const userMacros = await import(macrosPath);
 
-  const userMacros = require(macrosPath);
-  //   const userMacros: ModuleExports = require(macrosPath);
-  //   const macros = new Map<string, MacroFunction>();
+  for (const key in userMacros) {
+    const macro = userMacros[key];
+    if (typeof macro === "function" && MACRO_IDENTIFIER in macro) {
+      functionMap.set(macro.macro_identifier, macro);
+    }
+  }
 
-  console.log(userMacros);
-
-  // User must have a "macro_identifier" property on any function that they want to use as a macro
-  //   Object.keys(userMacros).forEach(([key, value]) => {
-  //     if (value.hasOwnProperty(_MACRO_IDENTIFIER)) {
-  //       // macros.set(value[_MACRO_IDENTIFIER], userMacros[key]);
-  //     }
-  //   });
-
-  //   return macros;
+  const test = functionMap.get("test");
+  if (test) {
+    console.log(test());
+  }
 }
-
-let x = 1;
