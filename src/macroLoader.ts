cb807751b31c3e813be7193d27cf9f9615650497
Mike Jovanovich <mpjovanovich@gmail.com>
2024-06-14 16:20:00 -0400

    Can compile now.

--- src/macroLoader.ts
+++ src/macroLoader.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-// const _MACRO_IDENTIFIER = "macro_identifier";
+const _MACRO_IDENTIFIER = "macro_identifier";
 
 // // export interface MacroFunction {
 // //   (args: string[], text: string): string;
@@ -14,26 +14,27 @@ import path from "path";
 //   };
 // }
 
-// export function loadMacros(macrosPath: string): void {
-//   // export function loadMacros(macrosPath: string): Map<string, MacroFunction> {
-//   // Check if the file exists
-//   if (!fs.existsSync(macrosPath)) {
-//     throw new Error(`File does not exist: ${macrosPath}`);
-//   }
+export function loadMacros(macrosPath: string): void {
+  // export function loadMacros(macrosPath: string): Map<string, MacroFunction> {
+  // Check if the file exists
+  if (!fs.existsSync(macrosPath)) {
+    throw new Error(`File does not exist: ${macrosPath}`);
+  }
 
-//   const userMacros: ModuleExports = require(macrosPath);
-//   //   const macros = new Map<string, MacroFunction>();
+  const userMacros = require(macrosPath);
+  //   const userMacros: ModuleExports = require(macrosPath);
+  //   const macros = new Map<string, MacroFunction>();
 
-//   console.log(userMacros);
+  console.log(userMacros);
 
-//   // User must have a "macro_identifier" property on any function that they want to use as a macro
-//   //   Object.keys(userMacros).forEach(([key, value]) => {
-//   //     if (value.hasOwnProperty(_MACRO_IDENTIFIER)) {
-//   //       // macros.set(value[_MACRO_IDENTIFIER], userMacros[key]);
-//   //     }
-//   //   });
+  // User must have a "macro_identifier" property on any function that they want to use as a macro
+  //   Object.keys(userMacros).forEach(([key, value]) => {
+  //     if (value.hasOwnProperty(_MACRO_IDENTIFIER)) {
+  //       // macros.set(value[_MACRO_IDENTIFIER], userMacros[key]);
+  //     }
+  //   });
 
-//   //   return macros;
-// }
+  //   return macros;
+}
 
 let x = 1;
