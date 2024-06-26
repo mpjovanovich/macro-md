f2fe565a91f5e2bbeb4f23f372d5c49ff263fc51
Mike Jovanovich <mpjovanovich@gmail.com>
2024-06-26 10:03:13 -0400

    Changes for VS Code test harness.

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,30 @@
-import { parse } from "./macroLoader.js";
+import {
+  MacroCall,
+  embedTokens,
+  escapeRegExp,
+  loadMacros,
+  parse,
+} from "./macroLoader.js";
 
-// This is a sandbox file to test the macroLoader. It is not part of the main project.
-const result = await parse(
-  "/home/mpjovanovich/git/macro-md/test/markdown/test.md",
-  "/home/mpjovanovich/git/macro-md/test/macro/testMacro.js",
-  "^"
+const macroDelimiter = "^";
+const escapedMacroDelimiter = escapeRegExp(macroDelimiter);
+const macroRegex = new RegExp(
+  `${escapedMacroDelimiter}\\s*(\\S+?)\\s*(?:\\((.*?)\\))?\\s*\\{`,
+  "g"
 );
-console.log(result);
+const guid = `macro_md_${Date.now().toString()}`;
+let placeholders = new Map<string, MacroCall>();
+
+const macroPath = "/home/mpjovanovich/git/macro-md/test/macro/testMacro.js";
+const macros = await loadMacros(macroPath, (path) => import(path));
+let markdown = "start ^testNoArguments{first} ^testNoArguments{second} end";
+markdown = embedTokens(markdown, macroRegex, macros, placeholders, guid);
+console.log("dun");
+
+// This is a sandbox file for debugging. It is not part of the main project.
+// const result = await parse(
+//   "/home/mpjovanovich/git/macro-md/test/markdown/test.md",
+//   "/home/mpjovanovich/git/macro-md/test/macro/testMacro.js",
+//   "^"
+// );
+// console.log(result);
