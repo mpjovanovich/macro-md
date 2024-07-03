eeab342c84b6f74ab2e77247897ca553d14159b0
Jonny Danger <mpjovanovich@gmail.com>
2024-07-03 17:55:03 -0400

    More setup for my personal comparison to existing notes site.

--- src/test.ts
+++ src/test.ts
@@ -78,7 +78,30 @@ function writeHTMLFile() {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Markdown Test</title>
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="assets/images/favicon-32x32.png"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="assets/images/favicon-16x16.png"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;1,400;1,700&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,400;1,700&display=swap"
+            rel="stylesheet"
+        />
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="assets/css/highlight.css">
     </head>
